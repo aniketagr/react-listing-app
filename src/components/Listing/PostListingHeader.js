2bacb65d9eda43bfc36ec77234430fda80479516
Agrahari <aniket.agrahari@capgemini.com>
2021-08-02 10:15:35 +0530

    listing component

---
+++ src/components/Listing/PostListingHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostListingHeader() {
+    return (
+        <React.Fragment>
+            <h1>Listing Post </h1>
+        </React.Fragment>
+    )
+}
+
+export default PostListingHeader
